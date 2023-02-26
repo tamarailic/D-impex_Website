1b42fbe149ad69a24d5e3e76b1281048b9579118
Uros Pocek <uros.pocek@gmail.com>
2023-02-26 15:14:29 +0100

    feat: Added responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 (function () {
-    var burger = document.querySelector('.burger-container'),
-        header = document.querySelector('.header');
+    let burger = document.querySelector('.burger-container');
+    let header = document.querySelector('.header');
+    let menu = document.querySelector('.menu')
 
     burger.onclick = function () {
         header.classList.toggle('menu-opened');
+        menu.classList.toggle('hide');
     }
 }());
