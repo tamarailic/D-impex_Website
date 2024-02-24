7162226c65520e1f5611e7c1e0b5b2902e8053c6
Uros Pocek <uros.pocek@gmail.com>
2024-02-24 14:14:28 +0100

    feat: Added faq in bazeni srb

--- js/main.js
+++ js/main.js
@@ -34,3 +34,21 @@ function changeLanguageMobile() {
 
     window.location.href = newUrl;
 }
+
+// FAQ
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+    const itemToggle = this.getAttribute('aria-expanded');
+
+    for (i = 0; i < items.length; i++) {
+        items[i].setAttribute('aria-expanded', 'false');
+    }
+
+    if (itemToggle == 'false') {
+        this.setAttribute('aria-expanded', 'true');
+    }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
